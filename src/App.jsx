c004e199fdf9c4b09cf5c219b798071bff528744
FedericoBen <federicobg95@gmail.com>
2024-10-10 14:52:19 -0500

    [FIX] Width sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,3 @@ function App() {
 }
 
 export default App;
-
-// const [data, setData] = useState(null);
-
-// useEffect(() => {
-//   fetch("https://htj2hjvrqc.execute-api.us-east-1.amazonaws.com/dev/users", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   })
-//     .then((resp) => resp.json())
-//     .then((resp) => setData(resp))
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// }, []);
